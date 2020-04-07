5bf6566bd5529cf6f4d3404f582294672a836c79
Mateus Henrique <hi@mateus.cc>
2020-04-07 13:55:06 -0300

    feat: add Companies layout

--- src/pages/Companies/Layout/Companies.styled.ts
+++ src/pages/Companies/Layout/Companies.styled.ts
@@ -2,4 +2,29 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   padding-top: 20.6rem;
+
+  header {
+    margin-bottom: 17.5rem;
+  }
 `;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  max-width: 1263px;
+  background-color: black;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 31.5rem;
+  height: 20rem;
+`;
+
+export const Asset = styled.img``;
--- src/pages/Companies/Layout/CompaniesLayout.tsx
+++ src/pages/Companies/Layout/CompaniesLayout.tsx
@@ -3,18 +3,34 @@ import { useTranslation } from 'react-i18next';
 
 import { Heading } from 'components';
 
-import { Container } from './Companies.styled';
+import { Container, List, ListItem, Asset } from './Companies.styled';
 
 interface Props {
   id: string;
 }
 
+interface ListProps {
+  picture: string;
+  alt: string;
+}
+
 const CompaniesLayout: React.FC<Props> = (props: Props) => {
   const { t } = useTranslation('companies');
+
+  const list: ListProps[] = t('list', {
+    returnObjects: true,
+  });
 
   return (
     <Container {...props}>
       <Heading label={t('title')} />
+      <List>
+        {list.map((item, i: number) => (
+          <ListItem key={`company-${i}`}>
+            <Asset src={require(`res/images/companies/${item.picture}`)} />
+          </ListItem>
+        ))}
+      </List>
     </Container>
   );
 };
