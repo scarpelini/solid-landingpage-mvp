c588112fb532479b2410e5c7cfda91d1bc03de9d
Mateus Henrique <hi@mateus.cc>
2020-04-07 17:00:21 -0300

    chore: menu component

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,8 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Link } from 'react-router-dom';
 
-export const Image = styled.img``;
+interface MenuProps {
+  toggle?: boolean;
+}
+
+export const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 1770px;
+  width: 100%;
+`;
+
+export const Image = styled.img`
+  position: relative;
+  z-index: 11;
+`;
 
 export const Container = styled.header`
   background-color: ${({ theme: { colors } }) => colors.background};
@@ -13,9 +29,8 @@ export const Container = styled.header`
   top: 0;
   left: 50%;
   z-index: 10;
-  padding-top: 5rem;
+  padding: 3rem 0;
   transform: translateX(-50%);
-  max-width: 1770px;
   width: 100%;
 `;
 
@@ -60,10 +75,11 @@ export const ButtonBookNow = styled(Link)`
   letter-spacing: 0.69pt;
 `;
 
-export const Hamb = styled.button`
+export const Hamb = styled.button<MenuProps>`
   display: block;
   position: relative;
   top: 3px;
+  z-index: 11;
   width: 4.2rem;
   height: 2rem;
 
@@ -77,22 +93,110 @@ export const Hamb = styled.button`
     display: inline-block;
     width: 4.2rem;
     height: 0.2rem;
+    transition: transform 0.25s ease-in-out 0s, top 0.25s ease-in-out 0s,
+      left 0.25s ease-in-out 0s;
   }
 
   &:after {
     top: 9px;
   }
+
+  ${({ toggle }) =>
+    toggle &&
+    css`
+      &:before {
+        top: 3px;
+        transform: rotate(45deg);
+      }
+
+      &:after {
+        top: 3px;
+        transform: rotate(-45deg);
+      }
+    `}
+`;
+
+export const Toggle = styled.div<MenuProps>`
+  display: flex;
+  background-color: ${({ theme: { colors } }) => colors.background};
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  width: 100%;
+  height: 0;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  transition: height 0.25s ease-in-out 0s;
+
+  ${({ toggle }) =>
+    toggle &&
+    css`
+      height: 100vh;
+    `}
 `;
 
 export const List = styled.ul`
-  background-color: ${({ theme: { colors } }) => colors.background};
-  display: none;
+  display: flex;
   flex-direction: column;
-  position: absolute;
-  top: 0;
-  left: 0;
+  margin: 0 auto;
+  padding-left: 15rem;
+  max-width: 1281px;
   width: 100%;
-  height: 100%;
+  height: auto;
 `;
 
-export const ListItem = styled.li``;
+export const ListItem = styled.li`
+  &:first-child {
+    margin-top: 10rem;
+  }
+
+  &:last-child {
+    margin-bottom: 10rem;
+  }
+
+  &:hover {
+    a {
+      color: ${({ theme: { colors } }) => colors.primary};
+      font-size: 11rem;
+    }
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 16.3rem;
+  }
+
+  a {
+    color: ${({ theme: { colors } }) => colors.onSecondary};
+    font-family: ${({ theme: { typography } }) => typography.h1.fontFamily};
+    font-size: ${({ theme: { typography } }) => typography.h1.fontSize};
+    display: block;
+    text-decoration: none;
+    white-space: nowrap;
+    transition: font-size 0.25s ease-in-out 0s, color 0.25s ease-in-out 0s;
+  }
+`;
+
+export const Socials = styled.ul<MenuProps>`
+  opacity: ${({ toggle }) => (toggle ? 1 : 0)};
+  visibility: ${({ toggle }) => (toggle ? 'visible' : 'hidden')};
+  display: flex;
+  flex-direction: row;
+  position: fixed;
+  left: 50%;
+  bottom: 58px;
+  transform: translateX(-50%);
+  z-index: 10;
+  max-width: 1770px;
+  width: 100%;
+  transition: opacity 0.25s ease-in-out 0.5s, visibility 0.25s ease-in-out 0s;
+  pointer-events: none;
+`;
+
+export const SocialItem = styled.li`
+  pointer-events: auto;
+
+  &:not(:last-child) {
+    margin-right: 5.1rem;
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { changeLanguage, changeTheme } from 'redux/modules/settings/actions';
 // import { getLanguage, getTheme } from 'redux/modules/settings/selectors';
@@ -6,20 +6,26 @@ import { useTranslation } from 'react-i18next';
 import { HashLink as Link } from 'react-router-hash-link';
 
 import Logo from 'res/svg/solid-logotype.svg';
+import { A } from 'components';
 import {
   Container,
   Image,
   Text,
   Nav,
   Hamb,
+  Wrap,
+  Toggle,
   List,
   ListItem,
   ButtonBookNow,
+  Socials,
+  SocialItem,
 } from './Header.styled';
 
 interface Props {}
 
 const Header: React.FC<Props> = (props: Props) => {
+  const [openMenu, setOpenMenu] = useState(false);
   const { t } = useTranslation(['common', 'header']);
   // const dispatch = useDispatch();
   // const language = useSelector(getLanguage);
@@ -28,58 +34,72 @@ const Header: React.FC<Props> = (props: Props) => {
   // const nextTheme = currentTheme === 'light' ? 'Dark' : 'Light';
 
   return (
-    <Container>
-      <Image src={Logo} />
-      <Nav>
-        <Text>{t('header:investment')}</Text>
-        <Text>{t('header:immediateAvailability')}</Text>
-        <ButtonBookNow to={'/'}>{t('common:button.bookNow')}</ButtonBookNow>
-        <Hamb />
-        <List>
-          <ListItem>
-            <Link smooth to={'/'}>
-              {t('menu.0')}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link smooth to={'/#our-expertise'}>
-              {t('menu.1')}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link smooth to={'/#our-manifesto'}>
-              {t('menu.2')}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link smooth to={'/#tech-stack'}>
-              {t('menu.3')}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link smooth to={'/#partners-say'}>
-              {t('menu.4')}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link smooth to={'/#companies'}>
-              {t('menu.5')}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link smooth to={'/#say-hi'}>
-              {t('menu.6')}
-            </Link>
-          </ListItem>
-        </List>
-      </Nav>
-      {/* <A onClick={() => dispatch(changeLanguage(nextLanguage))}>
+    <>
+      <Container>
+        <Wrap>
+          <Image src={Logo} />
+          <Nav>
+            <Text>{t('header:investment')}</Text>
+            <Text>{t('header:immediateAvailability')}</Text>
+            <ButtonBookNow to={'/'}>{t('common:button.bookNow')}</ButtonBookNow>
+            <Hamb onClick={() => setOpenMenu(!openMenu)} toggle={openMenu} />
+          </Nav>
+        </Wrap>
+        {/* <A onClick={() => dispatch(changeLanguage(nextLanguage))}>
         {nextLanguage}
       </A>
       <A onClick={() => dispatch(changeTheme(nextTheme.toLowerCase()))}>
         {nextTheme}
       </A> */}
-    </Container>
+        <Toggle toggle={openMenu}>
+          <List>
+            <ListItem>
+              <Link smooth to={'/'}>
+                {t('header:menu.0')}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link smooth to={'/#our-expertise'}>
+                {t('header:menu.1')}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link smooth to={'/#our-manifesto'}>
+                {t('header:menu.2')}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link smooth to={'/#tech-stack'}>
+                {t('header:menu.3')}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link smooth to={'/#partners-say'}>
+                {t('header:menu.4')}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link smooth to={'/#companies'}>
+                {t('header:menu.5')}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link smooth to={'/#say-hi'}>
+                {t('header:menu.6')}
+              </Link>
+            </ListItem>
+          </List>
+        </Toggle>
+      </Container>
+      <Socials toggle={openMenu}>
+        <SocialItem>
+          <A href={'#'}>LinkedIn</A>
+        </SocialItem>
+        <SocialItem>
+          <A href={'#'}>Instagram</A>
+        </SocialItem>
+      </Socials>
+    </>
   );
 };
 
