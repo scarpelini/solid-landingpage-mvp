6d147b4393e01d7b8e969d2da8cdb948fe11e77f
Julio Scarpelini <julio@santa.io>
2021-01-12 15:13:14 -0300

    Added Webpack 4 package and Facebook Pixel to the Home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -88,10 +88,24 @@ class MyDocument extends Document {
                 };
               g._v = '1.2.0';
             })(window, document, window['_fs_namespace'], 'script', 'user');
+
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '872569686875530');
+            fbq('track', 'PageView');
           `,
           }}
         />
-
+        <noscript><img height="1" width="1" style={{ display:"none" }}
+        src="https://www.facebook.com/tr?id=872569686875530&ev=PageView&noscript=1"
+        /></noscript>
         <Head />
 
         <title>SOLID Design and Development for Startups</title>
